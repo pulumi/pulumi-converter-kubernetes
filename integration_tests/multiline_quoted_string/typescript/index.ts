5a9371a14706b2ba3b410b11f8904d972e7dc4a3
Ramon Quitales <ramon@pulumi.com>
2024-03-05 16:56:15 -0800

    tests: add integration test for multiline quoted strings

---
+++ integration_tests/multiline_quoted_string/typescript/index.ts
@@ -0,0 +1,33 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as kubernetes from "@pulumi/kubernetes";
+
+const kubecostPrometheusServer = new kubernetes.core.v1.ConfigMap("kubecostPrometheusServer", {
+    data: {
+        "prometheus.yml": `global:
+  evaluation_interval: 1m
+  external_labels:
+    cluster_id: 'A'
+    port: 9003
+- job_name: kubecost-networking
+  kubernetes_sd_configs:
+    - role: pod
+  relabel_configs:
+  # Scrape only the the targets matching the following metadata
+    - source_labels: [__meta_kubernetes_pod_label_app]
+      action: keep
+      regex:  kubecost-network-costs
+
+`,
+    },
+    metadata: {
+        labels: {
+            app: "prometheus",
+            chart: "prometheus-11.0.2",
+            component: "server",
+            heritage: "Helm",
+            release: "kubecost",
+        },
+        name: "kubecost-prometheus-server",
+        namespace: "kubecost",
+    },
+});
